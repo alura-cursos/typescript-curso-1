5501fd373cd2162ffe35e9c6e4c6281b7ba6edf2
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-07-26 19:54:58 -0300

    feat: :sparkles: Herança

--- app/views/alert-view.ts
+++ app/views/alert-view.ts
@@ -1,13 +1,9 @@
 import { View } from "./view.js";
 
-export class AlertView extends View {
+export class AlertView extends View<string> {
   template(model: string): string {
     return `
       <p class='alert alert-info'>${model}</p>
     `;
-  }
-  update(model: string): void {
-    const template = this.template(model);
-    this.element.innerHTML = template;
   }
 }
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,4 +2,8 @@ export class View {
     constructor(seletor) {
         this.element = document.querySelector(seletor);
     }
+    update(model) {
+        const template = this.template(model);
+        this.element.innerHTML = template;
+    }
 }
